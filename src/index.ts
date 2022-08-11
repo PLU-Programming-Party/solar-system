77105fb07e0943a0d3a5c0efb0fdefc1029516bb
Kieran <kimmurkj@plu.edu>
2022-08-10 22:02:17 -0700

    feat: raycast to spatial bodies
    update keplar elements in ui to match current spatial body
    add xanadu

--- src/index.ts
+++ src/index.ts
@@ -19,12 +19,10 @@ type SpatialEntity = {
 }
 
 const _entities: SpatialEntity[] = [];
+const _keplarElementMap = new Map<SpatialEntity, KeplarElements>();
 
+//TODO: Live update keplar elements
 //TODO: Create a more comprehensive testing suite
-
-function configureGUI(){
-  //TODO: refactor GUI
-}
 
 const sceneParams = {
   pauseScene: false,
@@ -39,14 +37,17 @@ sceneGUI.add( sceneParams, 'updateOrbit').name('Update Orbit');
 
 let ps = new PlanetarySystem();
 
-const keplarElements: KeplarElements = {
+let activeEntity: SpatialEntity = null;
+const activeKeplarElements: KeplarElements = {
   eccentricity: 0,
   semi_major_axis: 250,
   inclination: 0,
   ascending_node: 0,
   periapsis: 0,
   true_anomaly: 0
-};
+}
+
+const earthElements: KeplarElements = {...activeKeplarElements};
 
 function appendNewEntity(body: SpatialBody, group: THREE.Group): SpatialEntity {
   scene.add(group);
@@ -60,6 +61,7 @@ function appendNewEntity(body: SpatialBody, group: THREE.Group): SpatialEntity {
     orbit,
     updateOrbit
   }
+  group.userData.entity = entity;
   _entities.push(entity);
   return entity;
 }
@@ -69,34 +71,80 @@ const sun = ps.constructBody(sunMass, new THREE.Vector3(0, 0, 0), new THREE.Vect
 appendNewEntity(sun, createSunMesh(sunMass));
 
 const earthMass = 500;
-const earth = ps.constructBodyRelative(earthMass, sun, keplarElements);
-appendNewEntity(earth, createEarthMesh(earthMass));
+const earth = ps.constructBodyRelative(earthMass, sun, earthElements);
+const earthEntity = appendNewEntity(earth, createEarthMesh(earthMass));
+activeEntity = earthEntity;
+_keplarElementMap.set(earthEntity, earthElements);
+
+const xanElements: KeplarElements = {
+  eccentricity: 0,
+  semi_major_axis: 400,
+  inclination: 0,
+  ascending_node: 0,
+  periapsis: 0,
+  true_anomaly: 0
+};
+
+const xanMass = 750;
+const xan = ps.constructBodyRelative(xanMass, sun, xanElements);
+const xanEntity = appendNewEntity(xan, createEarthMesh(xanMass));
+_keplarElementMap.set(xanEntity, xanElements);
 
 scene.add(createStarField(Math.random(), 10));
 
-
 const fixedInterval = 20; // Interval time in milliseconds
-let intervals = 2 * Math.PI * Math.sqrt(Math.pow(keplarElements.semi_major_axis, 3) / (G * sun.mass)) / fixedInterval;
+let intervals = 2 * Math.PI * Math.sqrt(Math.pow(activeKeplarElements.semi_major_axis, 3) / (G * sun.mass)) / fixedInterval;
 
 const keplarGui = gui.addFolder("Keplar Elements");
 keplarGui.onChange(() => {
-  intervals = 2 * Math.PI * Math.sqrt(Math.pow(keplarElements.semi_major_axis, 3) / (G * sun.mass)) / fixedInterval;
-  const stateVectors = keplarToCartesian(sun, 500, keplarElements);
-  earth.pos.set(stateVectors.pos.x, stateVectors.pos.y, stateVectors.pos.z);
-  earth.vel.set(stateVectors.vel.x, stateVectors.vel.y, stateVectors.vel.z);
+  intervals = 2 * Math.PI * Math.sqrt(Math.pow(activeKeplarElements.semi_major_axis, 3) / (G * sun.mass)) / fixedInterval;
+  const stateVectors = keplarToCartesian(sun, 500, activeKeplarElements);
+  activeEntity.body.pos.set(stateVectors.pos.x, stateVectors.pos.y, stateVectors.pos.z);
+  activeEntity.body.vel.set(stateVectors.vel.x, stateVectors.vel.y, stateVectors.vel.z);
 })
-keplarGui.add(keplarElements, 'eccentricity', 0, 1).name('Eccentricity');
-keplarGui.add(keplarElements, 'semi_major_axis', 100, 1000).name('Semi-Major Axis');
-keplarGui.add(keplarElements, 'inclination', 0, 360).name('Inclination');
-keplarGui.add(keplarElements, 'ascending_node', 0, 360).name('Angle of Asc. Node');
-keplarGui.add(keplarElements, 'periapsis', 0, 360).name('Periapsis');
-keplarGui.add(keplarElements, 'true_anomaly', 0, 360).name('True Anomaly');
+keplarGui.add(activeKeplarElements, 'eccentricity', 0, 1).name('Eccentricity').listen();
+keplarGui.add(activeKeplarElements, 'semi_major_axis', 100, 1000).name('Semi-Major Axis').listen();
+keplarGui.add(activeKeplarElements, 'inclination', 0, 360).name('Inclination').listen();
+keplarGui.add(activeKeplarElements, 'ascending_node', 0, 360).name('Angle of Asc. Node').listen();
+keplarGui.add(activeKeplarElements, 'periapsis', 0, 360).name('Periapsis').listen();
+keplarGui.add(activeKeplarElements, 'true_anomaly', 0, 360).name('True Anomaly').listen();
 
 let ambient = new THREE.AmbientLight(0x333333);
 
 scene.add(ambient);
 camera.position.set(0, 0, 500);
 camera.lookAt(0,0,0); 
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+function onMouseClick(event: MouseEvent) {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(_entities.map(e => e.visual));
+  if (intersects.length > 0) {
+    const intersect = intersects[0];
+    let parent = intersect.object.parent;
+    let entity: SpatialEntity;
+    while(parent) {
+      if (parent.userData.entity) {
+        entity = parent.userData.entity;
+        break;
+      } else { 
+        parent = parent.parent;
+      }
+    }
+      
+    if (entity) {
+      const keplar = _keplarElementMap.get(entity);
+      Object.assign(activeKeplarElements, keplar);
+      activeEntity = entity;
+    }
+  }
+}
+
+window.addEventListener('click', onMouseClick);
 
 //animation frame for cube
 function animate() {
